9beeaadc9d4004d40511814953406354f86d53dc
Hossam Cj <hossamcj@gmail.com>
2022-08-31 16:31:41 +0200

    Created checkout component, checkout items and checkout functionality

--- 02-Udemy/3-Reactjs-ZTM/Projects/CRWN-Clothing-Store/src/App.js
+++ 02-Udemy/3-Reactjs-ZTM/Projects/CRWN-Clothing-Store/src/App.js
@@ -4,6 +4,7 @@ import Navigation from './routes/home/navigation/navigation.component'
 import Home from './routes/home/home.component'
 import SignIn from './routes/sign-in/sign-in.component'
 import Shop from './routes/shop/shop.component'
+import Checkout from './routes/checkout/checkout.component'
 
 
 
@@ -15,6 +16,7 @@ const App = () => {
         <Route path='home' element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='sign-in' element={<SignIn />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   ) 
--- 02-Udemy/3-Reactjs-ZTM/Projects/CRWN-Clothing-Store/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ 02-Udemy/3-Reactjs-ZTM/Projects/CRWN-Clothing-Store/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react'
 
+import { useNavigate } from 'react-router-dom'
+
 import { CartContext } from '../../contexts/cart.context';
 
 import CartItem from '../cart-item/cart-item.component';
@@ -9,6 +11,11 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext)
+  const navigate = useNavigate()
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout')
+  }
 
   return (
     <div className='cart-dropdown-container'>
@@ -17,7 +24,7 @@ const CartDropdown = () => {
           <CartItem cartItem={item} key={item.id} />
         ))}
       </div>
-      <Button>CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   )
 }
