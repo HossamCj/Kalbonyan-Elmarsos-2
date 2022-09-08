e1f6a5e8db34da50dcf9fa62b7569b4d6e577c47
Hossam Cj <hossamcj@gmail.com>
2022-09-08 21:32:15 +0200

    Working on the Firestore database storage

--- 02-Udemy/3-Reactjs-ZTM/Projects/CRWN-Clothing-Store/src/App.js
+++ 02-Udemy/3-Reactjs-ZTM/Projects/CRWN-Clothing-Store/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
       
         <Route path='home' element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        <Route path='shop/*' element={<Shop />} />
         <Route path='sign-in' element={<SignIn />} />
         <Route path='checkout' element={<Checkout />} />
       </Route>
